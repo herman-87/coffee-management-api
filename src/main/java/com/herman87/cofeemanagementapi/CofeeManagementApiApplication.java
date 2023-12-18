b92dc71f407100103f78593b9fca6aae2c493325
herman-87 <hermanessoungou@gmail.com>
2023-12-18 07:18:26 +0100

    OK

---
+++ src/main/java/com/herman87/cofeemanagementapi/CofeeManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.herman87.cofeemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CofeeManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CofeeManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herman87/cofeemanagementapi/CofeeManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herman87.cofeemanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofeeManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
