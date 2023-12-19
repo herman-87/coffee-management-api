0c33759d5c120bb544ba1957e94e4d22d1945d5d
herman-87 <hermanessoungou@gmail.com>
2023-12-19 22:55:54 +0100

    OK

---
+++ src/main/java/com/herman87/cofeemanagementapi/JWT/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.herman87.cofeemanagementapi.JWT;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+public class SecurityConfig {
+}
